03d8339cf2e9790faabb882ac58fd3206fceb6c5
Medeiros156 <101615020+Medeiros156@users.noreply.github.com>
2024-09-16 17:04:05 -0300

    a

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const options = {
   key: fs.readFileSync("key.pem"),
   cert: fs.readFileSync("cert.pem"),
 };
+const port = process.env.PORT || 4000;
 
-https.createServer(options, app).listen(3000, () => {
+https.createServer(options, app).listen(port, () => {
   console.log("HTTPS server running on port 3000");
 });
